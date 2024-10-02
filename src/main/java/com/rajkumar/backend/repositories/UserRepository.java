3978bb5cd73b20fbfbf314794be2aea582a23631
Raj Kumar <119596365+kumar-modukuri@users.noreply.github.com>
2024-10-02 13:22:32 +0530

    Delete src directory

--- src/main/java/com/rajkumar/backend/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rajkumar.backend.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.rajkumar.backend.models.User;
-
-public interface UserRepository extends MongoRepository<User,String>{}
